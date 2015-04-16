e19dbb3f74b430eab4b66cef0cf5c68d7d4f7b71
Tomasz Maczukin <tomasz@maczukin.pl>
2015-04-16 23:25:03 +0200

    Separate DLS and backup configuration

--- lib/simple_backup/dsl.rb
+++ lib/simple_backup/dsl.rb
@@ -1,42 +1,4 @@
 module SimpleBackup
-  TIMESTAMP = Time.new.strftime('%Y%m%d%H%M%S')
-  @@status = :failed
-  @@logger = Logger.instance
-
-  def self.status
-    @@status
-  end
-
-  def self.run(&block)
-    @@logger.info "Backup #{TIMESTAMP} started"
-
-    dsl = DSL.new
-
-    @@logger.scope_start :info, "Configuration"
-    dsl.instance_eval(&block)
-    dsl.prepare
-    @@logger.scope_end
-
-    dsl.run
-    dsl.cleanup
-    @@status = :succeed
-
-    @@logger.info "Backup #{TIMESTAMP} finished"
-  rescue StandardError => e
-    self.handle_exception(e)
-  ensure
-    dsl.notify if dsl
-    @@logger.info "Notifications for backup #{TIMESTAMP} finished"
-  end
-
-  def self.handle_exception(e)
-    @@logger.error "#{e.class} => #{e.message}"
-    @@logger.error "Backup #{TIMESTAMP} failed"
-    STDERR.puts "Error @ #{Time.new.strftime('%Y-%m-%dT%H:%M:%S')}"
-    STDERR.puts "#{e.inspect}"
-    STDERR.puts e.backtrace
-  end
-
   class DSL
     @@logger = Logger.instance
 
